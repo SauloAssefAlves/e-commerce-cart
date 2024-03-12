ea0d0388761053b03bfec2976bca6a0784cc9a3e
Saulo <saulo.assef98@gmail.com>
2024-03-12 18:26:06 -0300

    feat: created the layout of the home and shopping cart pages

--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -1,7 +1,30 @@
 export function Cart() {
   return (
-    <div>
-      <h1>Page Cart</h1>
+    <div className='w-full max-w-7xl  mx-auto'>
+      <h1 className='font-medium text-2xl text-center my-4'>Shopping cart</h1>
+      <section className='flex items-center justify-between border-b-2 border-gray-300'>
+        <img
+          className='w-28'
+          src='https://www.kadri.com.br/sys/corteimg.asp?img=amp-1(42).jpg&sys=produtos&cut=1&w1=906&h1=906'
+          alt='Product logo'
+        />
+        <strong>Price: $1.000</strong>
+        <div className="flex items-center justify-center gap-3">
+          <button className='bg-slate-600 px-2 rounded text-white font-medium flex items-center justify-center'>
+            -
+          </button>
+          2
+          <button className='bg-slate-600 px-2 rounded text-white font-medium flex items-center justify-center'>
+            +
+          </button>
+        </div>
+
+        <strong className="float-right">
+          Subtotal: $1.000
+        </strong>
+      </section>
+
+      <p className="font-bold mt-4">Total: $1.000</p>
     </div>
   );
 }
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,30 @@
+import { BsCartPlus } from "react-icons/bs";
+
 export function Home() {
   return (
-    <div>
-      <h1>Page Home</h1>
+    <div className=''>
+      <main className='w-full max-w-7xl px-4 mx-auto'>
+        <h1 className='font-bold text-2xl mb-4 mt-10 text-center'>
+          Trending products
+        </h1>
+
+        <div className='grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-5'>
+          <section className='w-full'>
+            <img
+              className='w-full rounded-lg max-h-70 mb-2'
+              src='https://www.kadri.com.br/sys/corteimg.asp?img=amp-1(42).jpg&sys=produtos&cut=1&w1=906&h1=906'
+              alt='Product logo'
+            />
+            <p className='font-medium mt-1 mb-2'>Xbox Controller</p>
+            <div className='flex gap-3 items-center'>
+              <strong className='text-zinc-700/99'>$ 1.000</strong>
+              <button className='bg-zinc-900 p-1 rounded'>
+                <BsCartPlus size={20} color='#FFF' />
+              </button>
+            </div>
+          </section>
+        </div>
+      </main>
     </div>
   );
 }
