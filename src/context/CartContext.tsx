0faa0683ffc8f12ef20c909dc093cc9c7a055ac1
Saulo <saulo.assef98@gmail.com>
2024-03-15 14:13:40 -0300

    feat: add and remove products from the shopping cart

--- src/context/CartContext.tsx
+++ src/context/CartContext.tsx
@@ -5,6 +5,8 @@ interface CartContextData {
   cart: CartProps[];
   cartAmount: number;
   addProductCart: (newProduct: ProductProps) => void;
+  removeProductCart: (product: CartProps) => void;
+  total: string;
 }
 
 interface CartProps {
@@ -25,6 +27,8 @@ export const CartContext = createContext({} as CartContextData);
 
 function CartProvider({ children }: CartProviderProps) {
   const [cart, setCart] = useState<CartProps[]>([]);
+  const [total, setTotal] = useState("");
+
   function addProductCart(newProduct: ProductProps) {
     const indexProduct = cart.findIndex(
       (product) => product.id === newProduct.id
@@ -37,6 +41,7 @@ function CartProvider({ children }: CartProviderProps) {
         cartList[indexProduct].amount * cartList[indexProduct].price;
 
       setCart(cartList);
+      totalResultCart(cartList);
       return;
     }
 
@@ -47,10 +52,48 @@ function CartProvider({ children }: CartProviderProps) {
     };
 
     setCart((products) => [...products, data]);
+    totalResultCart([...cart, data]);
+  }
+
+  function removeProductCart(product: CartProps) {
+    const indexProduct = cart.findIndex((item) => item.id === product.id);
+
+    if (cart[indexProduct]?.amount > 1) {
+      let cartList = cart;
+      cartList[indexProduct].amount = cartList[indexProduct].amount - 1;
+      cartList[indexProduct].total =
+        cartList[indexProduct].total - cartList[indexProduct].price;
+
+      setCart(cartList);
+      totalResultCart(cartList);
+      return;
+    }
+
+    const removeProduct = cart.filter((item) => item.id !== product.id);
+    setCart(removeProduct);
+    totalResultCart(removeProduct);
+  }
+
+  function totalResultCart(products: CartProps[]) {
+    let myCart = products;
+    let result = myCart.reduce((acc, obj) => {
+      return acc + obj.total;
+    }, 0);
+    const resultFormat = result.toLocaleString("en-Us", {
+      style: "currency",
+      currency: "USD",
+    });
+    setTotal(resultFormat);
   }
   return (
     <CartContext.Provider
-      value={{ cart, cartAmount: cart.length, addProductCart }}>
+      value={{
+        cart,
+        cartAmount: cart.length,
+        addProductCart,
+        removeProductCart,
+        total,
+      }}>
       {children}
     </CartContext.Provider>
   );
--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -1,30 +1,65 @@
+import { useContext } from "react";
+import { CartContext } from "../../context/CartContext";
+import { Link } from "react-router-dom";
+
 export function Cart() {
+  const { cart, total, removeProductCart, addProductCart } =
+    useContext(CartContext);
   return (
-    <div className='w-full max-w-7xl  mx-auto'>
+    <div className='w-full max-w-7xl  mx-auto px-5 '>
       <h1 className='font-medium text-2xl text-center my-4'>Shopping cart</h1>
-      <section className='flex items-center justify-between border-b-2 border-gray-300'>
-        <img
-          className='w-28'
-          src='https://www.kadri.com.br/sys/corteimg.asp?img=amp-1(42).jpg&sys=produtos&cut=1&w1=906&h1=906'
-          alt='Product logo'
-        />
-        <strong>Price: $1.000</strong>
-        <div className="flex items-center justify-center gap-3">
-          <button className='bg-slate-600 px-2 rounded text-white font-medium flex items-center justify-center'>
-            -
-          </button>
-          2
-          <button className='bg-slate-600 px-2 rounded text-white font-medium flex items-center justify-center'>
-            +
-          </button>
+
+      {cart.length === 0 && (
+        <div className='flex flex-col items-center justify-center '>
+          <p className='font-medium'>Ops... Your cart is empty.</p>
+          <Link
+            to='/'
+            className='bg-slate-600 my-3 text-white font-medium rounded px-3 p-1'>
+            Acess products
+          </Link>
         </div>
+      )}
+      {cart.map((product) => (
+        <section
+          key={product.id}
+          className='flex items-center justify-between border-b-2 border-gray-300'>
+          <img className='w-28' src={product.cover} alt={product.title} />
+          <strong>
+            Price:{" "}
+            {product.price.toLocaleString("en-Us", {
+              style: "currency",
+              currency: "USD",
+            })}
+          </strong>
+          <div className='flex items-center justify-center gap-3'>
+            <button
+              onClick={() => {
+                removeProductCart(product);
+              }}
+              className='bg-slate-600 px-2 rounded text-white font-medium flex items-center justify-center'>
+              -
+            </button>
+            {product.amount}
+            <button
+              onClick={() => {
+                addProductCart(product);
+              }}
+              className='bg-slate-600 px-2 rounded text-white font-medium flex items-center justify-center'>
+              +
+            </button>
+          </div>
 
-        <strong className="float-right">
-          Subtotal: $1.000
-        </strong>
-      </section>
+          <strong className='float-right'>
+            Subtotal:{" "}
+            {product.total.toLocaleString("en-Us", {
+              style: "currency",
+              currency: "USD",
+            })}
+          </strong>
+        </section>
+      ))}
 
-      <p className="font-bold mt-4">Total: $1.000</p>
+      {cart.length !== 0 && <p className='font-bold mt-4'>Total: {total}</p>}
     </div>
   );
 }
